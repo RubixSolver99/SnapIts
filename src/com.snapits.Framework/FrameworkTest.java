42ec74c37ab9a7eb58d22286a396ff3af2445b6e
RubixSolver99 <blackbattiger@outlook.com>
2018-12-13 21:19:54 -0600

    NO SPACES!!!!!

--- src/com.snapits.Framework/FrameworkTest.java
+++ src/com.snapits.Framework/FrameworkTest.java
@@ -1,5 +1,5 @@
 public class FrameworkTest{
-    public static void main(String args[]){
-        System.out.println("Hello Framework!");
-    }
+	public static void main(String args[]){
+		System.out.println("Hello Framework!");
+	}
 }
