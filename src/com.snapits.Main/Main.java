4b142fd941fec4854df214f139577d13c2be16c8
RubixSolver99 <blackbattiger@outlook.com>
2018-12-15 12:11:58 -0600

    Added the Main JFrame

---
+++ src/com.snapits.Main/Main.java
@@ -0,0 +1,56 @@
+package com.snapits.Main;
+
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class Main extends JFrame {
+    private static final long serialVersionUID = 1L;
+    public static JFrame MainFrame; // not the regular kind
+    public static JPanel panelMain;
+
+    public Main() {
+		createView(getGraphics());
+		setTitle("Snap Its");
+		setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setResizable(false);
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		pack();
+	}
+
+	public void createView(Graphics g) {
+		panelMain = new JPanel();
+
+		getContentPane().add(panelMain);
+	}
+
+	public static void main(String[] Args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				MainFrame = new Main();
+				MainFrame.setVisible(true);
+			}
+		});
+
+	}
+
+}
