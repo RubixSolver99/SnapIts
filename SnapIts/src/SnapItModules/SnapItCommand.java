c2caf4496381035ffaa3d171d5b7386f24374047
RubixSolver99 <blackbattiger@outlook.com>
2018-12-28 13:46:52 -0600

    Fixed the lagging on the drag.
    The drag still isn't perfect, but it's a lot better.

--- SnapIts/src/SnapItModules/SnapItCommand.java
+++ SnapIts/src/SnapItModules/SnapItCommand.java
@@ -5,11 +5,17 @@ import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.MouseInfo;
+import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
 
 import javax.swing.JButton;
+import javax.swing.Timer;
 
 import GUI.GUI;
 
@@ -19,7 +25,6 @@ public class SnapItCommand extends JButton implements MouseListener {
 
 	public Color normalColor = null;
 
-	int buttonX, buttonY;
 	int mouseX, mouseY;
 	int offsetX, offsetY;
 
@@ -38,7 +43,10 @@ public class SnapItCommand extends JButton implements MouseListener {
 		setContentAreaFilled(false);
 	}
 
+	@Override
 	public void paintComponent(Graphics g) {
+		int cornerRadius = 10;
+
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
 
@@ -50,7 +58,17 @@ public class SnapItCommand extends JButton implements MouseListener {
 
 		// Draws the rounded opaque panel with borders
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // For High quality
-		g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 7, 7);
+
+		Area a = new Area(new Rectangle(0, cornerRadius, getWidth(), getHeight() - (cornerRadius * 2)));
+		a.add(new Area(new Rectangle(cornerRadius, 0, getWidth() - (cornerRadius * 2), getHeight())));
+		a.add(new Area(new Ellipse2D.Double(0, 0, cornerRadius * 2, cornerRadius * 2)));
+		a.add(new Area(new Ellipse2D.Double(getWidth() - (cornerRadius * 2), 0, cornerRadius * 2, cornerRadius * 2)));
+		a.add(new Area(new Ellipse2D.Double(0, getHeight() - (cornerRadius * 2), cornerRadius * 2, cornerRadius * 2)));
+		a.add(new Area(new Ellipse2D.Double(getWidth() - (cornerRadius * 2), getHeight() - (cornerRadius * 2),
+				cornerRadius * 2, cornerRadius * 2)));
+		g2d.fill(a);
+
+		// g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 7, 7);
 
 		super.paintComponent(g);
 	}
@@ -72,23 +90,21 @@ public class SnapItCommand extends JButton implements MouseListener {
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-	    if (e.getButton() == MouseEvent.BUTTON1) {
-	        mouseDown = true;
-	        mouseX = (int) MouseInfo.getPointerInfo().getLocation().getX();
-	        mouseY = (int) MouseInfo.getPointerInfo().getLocation().getY();
-	        buttonX = getX();
-	        buttonY = getY();
-	        offsetX = mouseX - buttonX;
-	        offsetY = mouseY - buttonY;
-	        initThread();
-	    }
+		if (e.getButton() == MouseEvent.BUTTON1) {
+			mouseDown = true;
+			mouseX = (int) MouseInfo.getPointerInfo().getLocation().getX();
+			mouseY = (int) MouseInfo.getPointerInfo().getLocation().getY();
+			offsetX = mouseX - getX();
+			offsetY = mouseY - getY();
+			initThread();
+		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-	    if (e.getButton() == MouseEvent.BUTTON1) {
-	        mouseDown = false;
-	    }
+		if (e.getButton() == MouseEvent.BUTTON1) {
+			mouseDown = false;
+		}
 	}
 
 	private synchronized boolean checkAndMark() {
@@ -102,37 +118,45 @@ public class SnapItCommand extends JButton implements MouseListener {
 		if (checkAndMark()) {
 			new Thread() {
 				public void run() {
-					do {
-				        mouseX = (int) MouseInfo.getPointerInfo().getLocation().getX();
-				        mouseY = (int) MouseInfo.getPointerInfo().getLocation().getY();
-				        
-				        int tempX = mouseX - offsetX;
-				        int tempY = mouseY - offsetY;
-				        
-				        boolean moveX;
-				        boolean moveY;
-				        
-				        if (tempX >= 1 && tempX + getWidth() <= GUI.fullScreenWidth + 1) {
-				        	moveX = true;
-				        } else {
-				        	moveX = false;
-				        }
-				        if (tempY >= 36 && tempY + getHeight() <= GUI.fullScreenWidth + 1) {
-				        	moveY = true;
-				        } else {
-				        	moveY = false;
-				        }
-				        
-				        if (moveX && moveY) {
-				        	setLocation(tempX, tempY);
-				        } else if (!moveX && moveY) {
-				        	setLocation(getX(), tempY);
-				        } else if (moveX && !moveY) {
-				        	setLocation(tempX, getY());
-				        } else {
-				        	setLocation(getX(), getY());
-				        }
-					} while (mouseDown && mouseIn);
+					Timer timer = new Timer(1, new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							if (mouseDown && mouseIn) {
+								mouseX = (int) MouseInfo.getPointerInfo().getLocation().getX();
+								mouseY = (int) MouseInfo.getPointerInfo().getLocation().getY();
+
+								int tempX = mouseX - offsetX;
+								int tempY = mouseY - offsetY;
+
+								boolean moveX;
+								boolean moveY;
+
+								if (tempX >= 1 && tempX + getWidth() <= GUI.fullScreenWidth + 1) {
+									moveX = true;
+								} else {
+									moveX = false;
+								}
+								if (tempY >= 36 && tempY + getHeight() <= GUI.fullScreenWidth + 1) {
+									moveY = true;
+								} else {
+									moveY = false;
+								}
+
+								if (moveX && moveY) {
+									setLocation(tempX, tempY);
+								} else if (!moveX && moveY) {
+									setLocation(getX(), tempY);
+								} else if (moveX && !moveY) {
+									setLocation(tempX, getY());
+								} else {
+									setLocation(getX(), getY());
+								}
+								repaint();
+							}
+						}
+					});
+					timer.setInitialDelay(0);
+					timer.start();
 					isRunning = false;
 				}
 			}.start();
